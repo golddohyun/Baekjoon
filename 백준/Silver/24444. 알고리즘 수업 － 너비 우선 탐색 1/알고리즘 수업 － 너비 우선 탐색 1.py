bf7a0b63178b80d722d5f3729814ca4442070ab8
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-09-03 17:36:39 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 584 ms, Memory: 60768 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(150000)
+
+def bfs(start):
+    global cnt
+    queue = deque([start])
+    visited[start] = cnt
+    
+    while queue:
+        v = queue.popleft()
+        for nei in graph[v]:
+            if visited[nei] == 0:  # Check if the node is unvisited
+                queue.append(nei)
+                cnt += 1
+                visited[nei] = cnt
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)  # Initialize with zeroes
+cnt = 1
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(N+1): 
+    graph[i].sort()
+
+bfs(R)
+print(*visited[1:], sep='\n')
